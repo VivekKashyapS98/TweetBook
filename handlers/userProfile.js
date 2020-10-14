a8054bb526de621b9044170859aceca2bd9995c3
Vivek Kashyap <vivekyeskashyap@gmail.com>
2020-10-14 18:00:56 +0530

    Adding new features

--- handlers/userProfile.js
+++ handlers/userProfile.js
@@ -12,5 +12,29 @@ exports.getMessages = async function(req, res, next) {
 }
 
 exports.userFollow = async function(req, res, next) {
-    
+    try {
+        let user = await db.User.findById(req.params.id);
+        user.following.push(req.params.id2);
+        await user.save();
+        let user2 = await db.User.findById(req.params.id2);
+        user2.followers.push(req.params.id);
+        await user2.save();
+        return res.status(200).json({tweet: "posted"});
+    } catch (err) {
+        return next(err);
+    }
+}
+
+exports.userUnfollow = async function(req, res, next) {
+    try {
+        let user = await db.User.findById(req.params.id);
+        user.following.pop(req.params.id2);
+        await user.save();
+        let user2 = await db.User.findById(req.params.id2);
+        user2.followers.pop(req.params.id);
+        await user2.save();
+        return res.status(200).json({tweet: "deleted"});
+    } catch (err) {
+        return next(err);
+    }
 }
--- routes/userProfile.js
+++ routes/userProfile.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-const { getMessages, userFollow } = require('../handlers/userProfile');
+const { getMessages, userFollow, userUnfollow } = require('../handlers/userProfile');
 
 router.get('/',getMessages);
-router.route('/:id2').post(userFollow);
+router.route('/:id2')
+            .post(userFollow)
+            .delete(userUnfollow)
 
 module.exports = router;
